8ce18c06c161954e508b8076527766a668a0b599
Tanu Bisht <tbisht939@gmail.com>
2026-06-18 12:21:38 +0530

    feat: integrate Gemini AI to explain PDF content
    - Added @google/genai package for Gemini AI integration
    - Added dotenv for environment variable management
    - POST /upload now sends extracted PDF chunks to Gemini AI
    - Returns AI generated explanation of PDF content

--- server/index.js
+++ server/index.js
@@ -2,9 +2,16 @@ const express = require('express')
 const multer = require('multer')
 const pdfParse = require('pdf-parse')
 const fs = require('fs')
+require('dotenv').config();
+const {GoogleGenAI} = require('@google/genai');
+
 const app = express()
 
 const upload = multer({dest: "uploads/"})
+
+const ai = new GoogleGenAI({
+    apiKey: process.env.GEMINI_API_KEY
+})
 
 app.get('/',(req,res) =>{
     res.send('Server is Up');
@@ -12,17 +19,30 @@ app.get('/',(req,res) =>{
 
 app.post('/upload', upload.single("pdf"), async(req,res) =>{
     console.log(req.file);
-    const dataBuffer = fs.readFileSync(req.file.path)
-    const pdfData = await pdfParse(dataBuffer)
-    const text = pdfData.text
-    
-    const chunks = text.split('\n\n')
+    try{
+        const dataBuffer = fs.readFileSync(req.file.path)
+        const pdfData = await pdfParse(dataBuffer)
+        const text = pdfData.text
+        
+        //res.send(text)//for reading the data 2nd lecture
+        const chunks = text.split('\n\n')
+        console.log(chunks[0])
 
-    res.json({
-        totalChunks: chunks.length,
-        chunks
+    // res.json({
+    //     totalChunks: chunks.length,
+    //     chunks
+
+    const response = await ai.models.generateContent({
+        model:'gemini-2.5-flash-lite',
+        contents: `explain this pdf in simple text ${chunks[1]}`
+    })
+    res.send(response.text);
+    }catch(err){
+        console.log(err);
+        res.status(500).send(err)
+    }
 })
-})
+
 
 app.listen(3000,() =>{
     console.log("server is running on port 3000");

a7f2eef2544707d97ac59869f64039e29be22452
Tanu Bisht <tbisht939@gmail.com>
2026-06-30 12:15:01 +0530

    PDF chat app with Gemini embeddings

--- server/index.js
+++ server/index.js
@@ -13,30 +13,60 @@ const ai = new GoogleGenAI({
     apiKey: process.env.GEMINI_API_KEY
 })
 
+ async function createEmbedding(text) {
+        const response = await ai.models.embedContent({
+        model: 'gemini-embedding-2',
+        contents: text,
+    });
+
+
+    return response.embeddings[0].values;
+
+        };
+
+
 app.get('/',(req,res) =>{
     res.send('Server is Up');
 })
 
 app.post('/upload', upload.single("pdf"), async(req,res) =>{
-    console.log(req.file);
+    console.log(req.body);
     try{
         const dataBuffer = fs.readFileSync(req.file.path)
         const pdfData = await pdfParse(dataBuffer)
         const text = pdfData.text
         
         //res.send(text)//for reading the data 2nd lecture
-        const chunks = text.split('\n\n')
-        console.log(chunks[0])
+        const chunks = text.split('\n\n').filter((chunk) => chunk.trim() != '');
+        console.log(chunks);
+        //console.log(chunks[0])
+
+
+        const embedding = await createEmbedding(chunks[0]);
+        console.log(embedding);
+
+        const question = req.body.question;
+        console.log(question);
+        const matchedChunk = chunks.find((chunk)=> chunk.toLowerCase().includes('vocab'));
+
+
 
     // res.json({
     //     totalChunks: chunks.length,
     //     chunks
 
     const response = await ai.models.generateContent({
-        model:'gemini-2.5-flash-lite',
-        contents: `explain this pdf in simple text ${chunks[1]}`
+        model:'gemini-2.5-flash',
+        contents: `answer the question using context: ${matchedChunk} and Question is: ${question}`
     })
     res.send(response.text);
+    //only for checking the matchedChunk and response together
+    // res.json({
+    //     matchedChunk,
+    //     response: response.text
+    // })
+
+
     }catch(err){
         console.log(err);
         res.status(500).send(err)
